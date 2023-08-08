38cf56762a8092ecd67a65c59439c1b57b76e670
unknown <triansyah53@gmail.com>
2023-08-08 17:41:40 +0700

    8 Aug 2023

--- Testing/tes_google.py
+++ Testing/tes_google.py
@@ -1,11 +1,20 @@
-import time
-
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 
-driver = webdriver.Chrome()
+# Provide the full path to chromedriver.exe
+chrome_driver_path = r"C:\MybrowserPATH\chromedriver.exe"
 
+# Set the path to the chromedriver executable using the Service class
+service = Service(executable_path=chrome_driver_path)
+
+# Create the Chrome WebDriver instance
+driver = webdriver.Chrome(service=service)
+
+# Navigate to Google's homepage
 driver.get("https://www.google.com")
 
-time.sleep(5)
+# Wait for 5 seconds (you can remove this if you don't need it)
+driver.implicitly_wait(5)
 
+# Close the browser
 driver.quit()
