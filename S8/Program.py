dcabc358df4d19e99aac8b029b7f848e6dbfc5eb
unknown <triansyah53@gmail.com>
2023-08-15 18:07:18 +0700

    15 Aug 2023

--- S8/Program.py
+++ S8/Program.py
@@ -1,48 +1,85 @@
-from selenium import webdriver
+import pdb
+
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import Select
+from WebDriverManager import create_driver
+import time
+from Login import login
 
-options=webdriver.ChromeOptions()
-options.add_experimental_option('detach',True)
-options.add_argument("--start-maximized")
-driver= webdriver.Chrome(options=options)
-driver.get('http://s8.asyx.com')
-wait=WebDriverWait(driver,10)
+driver = create_driver()
+login(driver)
+wait=WebDriverWait(driver,3)
 
-
-driver.find_element(By.ID,'username').send_keys('asyxsupport')
-driver.find_element(By.ID,'password').send_keys('Asyx!4362023',Keys.ENTER)
-
-documents_link = wait.until(
-    EC.presence_of_element_located((By.PARTIAL_LINK_TEXT, 'Documents'))
-)
-
-# Check if the Documents link is present
-if documents_link.is_displayed():
-    print("Login Success")
-else:
-    print("Login Failed")
-
-
-#Klik docstore
-# docstore= wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR,'#root > div > div > header > div > div > div > div.css-1wy0on6 > div > svg')))
-# docstore.click()
-
-# Click the dropdown
-dropdown = driver.find_element(By.CSS_SELECTOR, '.css-yk16xz-control')
-dropdown.click()
-
-# Find and type "PT AGRINDO"
+# Find the dosctore element and type "PT AGRINDO"
 input_element = driver.find_element(By.CSS_SELECTOR, 'input[type="text"][aria-autocomplete="list"]')
 input_element.send_keys("PT AGRINDO")
+time.sleep(1)
+input_element.send_keys(Keys.ARROW_DOWN)
+input_element.send_keys(Keys.ENTER)
 
-#
-# # Klik administration
-# main_menu_link = wait.until(EC.element_to_be_clickable((By.CLASS_NAME, 'nav-link.nav-dropdown-toggle')))
-# main_menu_link.click()
-#
-# # Wait for the "Program Management" link to be clickable
-# program_management_link = wait.until(EC.element_to_be_clickable((By.PARTIAL_LINK_TEXT, 'programmanagement')))
-# program_management_link.click()
+time.sleep(3)
+# Klik administration
+main_menu_link = wait.until(EC.element_to_be_clickable((By.CLASS_NAME, 'nav-link.nav-dropdown-toggle')))
+main_menu_link.click()
+
+# Wait for the "Program Management" link to be clickable
+program_management_link = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR,
+                                                                 '#root > div > div > div > div > div > ul > li.nav-item.nav-dropdown.open > ul > li:nth-child(5) > a')))
+program_management_link.click()
+
+#click add
+time.sleep(1)
+add_button = driver.find_element(By.CSS_SELECTOR, 'button.btn-primary i.fa-plus')
+add_button.click()
+
+
+time.sleep(1)
+prog_name=driver.find_element(By.NAME,'name').send_keys('Dummy')
+prog_desc=driver.find_element(By.NAME,'description').send_keys('Dummy Desc')
+
+dropdown = driver.find_element(By.CSS_SELECTOR, "div.css-1wa3eu0-placeholder").click()
+
+
+#Choose IDR
+driver.find_element(By.CSS_SELECTOR,'#react-select-5-input').send_keys("IDR",Keys.ENTER)
+
+#click slider Collaboration,Buyer,Multiple Buyer,Seller
+enabuy=driver.find_element(By.XPATH,'//*[@id="root"]/div/div/div/main/div[2]/div/div/div/div/div[1]/form/div[5]/div/div/div[1]/form/div/div[1]/div[2]/div/div/table/tbody/tr[2]/td[1]/label/span').click()
+multibuy=driver.find_element(By.XPATH,'//*[@id="root"]/div/div/div/main/div[2]/div/div/div/div/div[1]/form/div[5]/div/div/div[1]/form/div/div[1]/div[2]/div/div/table/tbody/tr[2]/td[2]/label/span').click()
+enasel=driver.find_element(By.XPATH,'//*[@id="root"]/div/div/div/main/div[2]/div/div/div/div/div[1]/form/div[5]/div/div/div[1]/form/div/div[2]/div[2]/div/div/table/tbody/tr[2]/td[1]/label/span').click()
+time.sleep(1)
+collab=driver.find_element(By.XPATH,'//*[@id="root"]/div/div/div/main/div[2]/div/div/div/div/div[1]/form/div[4]/div[2]/div/div[2]/label/span').click()
+
+#Quote
+q_ena=driver.find_element(By.XPATH,'//*[@id="root"]/div/div/div/main/div[2]/div/div/div/div/div[1]/form/div[5]/div/div/div[2]/div/div[2]/div[2]/div/div/table/tbody/tr[1]/td[2]/label/span').click()
+q_liti=driver.find_element(By.XPATH,'//*[@id="root"]/div/div/div/main/div[2]/div/div/div/div/div[1]/form/div[5]/div/div/div[2]/div/div[2]/div[2]/div/div/table/tbody/tr[2]/td[2]/label/span').click()
+q_invcheck=driver.find_element(By.XPATH,'//*[@id="root"]/div/div/div/main/div[2]/div/div/div/div/div[1]/form/div[5]/div/div/div[2]/div/div[2]/div[2]/div/div/table/tbody/tr[3]/td[2]/label/span').click()
+q_lir=driver.find_element(By.XPATH,'//*[@id="root"]/div/div/div/main/div[2]/div/div/div/div/div[1]/form/div[5]/div/div/div[2]/div/div[2]/div[2]/div/div/table/tbody/tr[4]/td[2]/label/span').click()
+q_disaprov=driver.find_element(By.XPATH,'//*[@id="root"]/div/div/div/main/div[2]/div/div/div/div/div[1]/form/div[5]/div/div/div[2]/div/div[2]/div[2]/div/div/table/tbody/tr[5]/td[2]/label/span').click()
+q_pricecor=
+
+q_logo = driver.find_element(By.ID, 'react-select-6-input')
+q_logo.click()
+q_logo.send_keys('b', Keys.ENTER)
+
+
+#Order
+ord=driver.find_element(By.XPATH,'//*[@id="root"]/div/div/div/main/div[2]/div/div/div/div/div[1]/form/div[5]/div/div/div[2]/div/div[2]/div[1]/ul/li[2]/div').click()
+o_ena=driver.find_element()
+o_liti=driver.find_element()
+o_logo=driver.find_element()
+
+#DA
+da_pda=driver.find_element()
+da_pad
+da_dgc
+da_sig
+
+#RA
+
+#INV
+
+# XXX=driver.find_element(By.XPATH,'')
