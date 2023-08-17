b2c463c678975028dbb6297f4d28939add802fd5
Triansyah <triansyah53@gmail.com>
2023-08-17 09:09:45 +0700

    x

--- S8/WebDriverManager.py
+++
@@ -1,8 +0,0 @@
-from selenium import webdriver
-
-def create_driver():
-    options = webdriver.ChromeOptions()
-    options.add_experimental_option('detach', True)
-    options.add_argument("--start-maximized")
-    driver = webdriver.Chrome(options=options)
-    return driver
