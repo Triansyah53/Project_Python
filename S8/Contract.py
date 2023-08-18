5220c0ee9bcd66b6e7db2d62acf8c309aa78f72f
unknown <triansyah53@gmail.com>
2023-08-18 17:42:30 +0700

    18 Aug 2023

--- S8/Contract.py
+++ S8/Contract.py
@@ -1,7 +1,12 @@
+import pdb
+
 from Login import login
+from selenium.webdriver.common.by import By
 from WebDriverManager import create_driver
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.keys import Keys
+import time
 
 driver=create_driver()
 login(driver)
@@ -11,3 +16,17 @@ input_element.send_keys("PT AGRINDO")
 time.sleep(1)
 input_element.send_keys(Keys.ARROW_DOWN)
 input_element.send_keys(Keys.ENTER)
+
+# Klik administration
+main_menu_link = wait.until(EC.element_to_be_clickable((By.CLASS_NAME, 'nav-link.nav-dropdown-toggle')))
+main_menu_link.click()
+
+# Wait for the "Contracts" link to be clickable
+contract_link = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'a[href*="contract"]')))
+contract_link.click()
+
+#Add contract
+add_contract= wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR,'i.fa.fa-plus')))
+add_contract.click()
+
+pdb.set_trace()
